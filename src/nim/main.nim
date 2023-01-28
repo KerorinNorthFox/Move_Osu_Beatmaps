0b4e62beff7a15ac77c7adaf5bfeb281579e7f29
kerorinnf <kitakitsune.gingitsune.8220A@gmail.com>
2023-01-28 18:33:11 +0900

    プロシージャを他プログラムからでも呼び出せるように細かく分割した

--- src/nim/main.nim
+++ src/nim/main.nim
@@ -3,74 +3,77 @@ import
   std/parsecfg,
   std/strformat
 
-const configPath: string = "config.ini"
+const configFilePath: string = "config.ini"
 
 proc help*(error:string="") = # ヘルプを表示
   echo error
 
-proc progressReport*(text:string) = # 結果表示
-  echo "\n####################"
-  echo text
-  echo "####################\n"
+# proc progressReport*(text:string) = # 結果表示
+#   echo "\n########################"
+#   echo text
+#   echo "########################\n"
 
-proc makeConfigFile*(path:string) = # configを作成する
+proc makeConfigFile*(configPath:string, path:string): string = # configを作成する
   var dict: Config = newConfig()
   dict.setSectionKey("", "path", path)
   dict.writeConfig(configPath)
-  progressReport("Made new config file.")
+  return "[Success]: Made new config file."
 
-proc updateConfigFile*(path:string) = # config編集
+proc updateConfigFile*(configPath:string, path:string): string = # config編集
   if not configPath.fileExists:
-    makeConfigFile(path)
-    return
+    return makeConfigFile(configPath, path)
   var dict: Config = loadConfig(configPath)
   dict.setSectionKey("", "path", path)
   dict.writeConfig(configPath)
-  progressReport("Updated new config file.")
+  return "[Success]: Updated new config file."
 
-proc loadConfigFile*(): string = # configを読み込む
-  if not configPath.fileExists:
-    help(fmt"""Error: '{configPath}'  file does not exist.
-Try the command '--path *DirectoryPath*' to make config file and set path""")
-    quit(QuitSuccess)
+proc loadConfigFile*(configPath:string): string = # configからpathを読み込む
   var dict: Config = loadConfig(configPath)
+  let path = dict.getSectionValue("", "path")
+  return path
+
+proc returnPath(): string = # 読み込んだpathを返す
+  if not configFilePath.fileExists:
+    help(fmt"""[Error]: '{configFilePath}'  file does not exist. Try the command '--path "DirectoryPath"' to make config file and set path""")
+    quit(QuitSuccess)
   try:
-    let path = dict.getSectionValue("", "path")
+    let path: string = loadConfigFile(configFilePath)
+    echo fmt"[Success]: Get 'path' Value from '{configFilePath}'."
     return path
   except KeyError:
-    help(fmt"""Error: Keyword 'path' does not exist in '{configPath}'.
-Try the command '--path *DirectoryPath*' to set path.""")
+    help(fmt"""[Error]: Keyword 'path' does not exist in '{configFilePath}'. Try the command '--path "DirectoryPath"' to set path.""")
     quit(QuitSuccess)
 
 proc moveFile*(toPath:string) = # ファイル移動
-  discard
+  echo toPath
 
 proc main() =
   let cmdArgCount: int = paramCount()
   if cmdArgCount == 0:
-    help("Error: No CommandLine Args.")
+    help("[Error]: No CommandLine Args.")
 
   elif cmdArgCount == 1:
-    let cmdArg: string = paramStr(0)
+    let cmdArg: string = paramStr(1)
     if cmdArg == "--run": # ファイル移動実行
-      let path: string = loadConfigFile()
+      let path: string = returnPath()
       moveFile(path)
     elif cmdArg == "--help": # ヘルプ表示
       help()
     else:
-      help("Error: unexpected command: " & cmdArg)
+      help("[Error]: unexpected command in one args: " & cmdArg)
 
   elif cmdArgCount == 2:
-    let firstCmdArg: string = paramStr(0)
-    let secondCmdArg: string = paramStr(1)
+    let firstCmdArg: string = paramStr(1)
+    let secondCmdArg: string = paramStr(2)
     if firstCmdArg == "--path": # パス設定
-      updateConfigFile(secondCmdArg)
+      let msg: string = updateConfigFile(configFilePath, secondCmdArg)
+      echo msg
     else:
-      help("Error: unexpected command: " & firstCmdArg)
+      help("[Error]: unexpected command in two args: " & firstCmdArg)
 
   else: # コマンドライン引数多すぎ
-    help("Error: Too many CommandLine Args")
+    help("[Error]: Too many CommandLine Args")
 
 
 when isMainModule:
-  discard
+  main()
