00171446d67c6af61d415f49752d1510ed52576a
kerorinnf <kitakitsune.gingitsune.8220A@gmail.com>
2023-02-21 22:48:32 +0900

    gui画面をほとんど実装

--- src/gui.nim
+++ src/gui.nim
@@ -2,25 +2,72 @@ import
   nigui,
   ./utils
 
+type UI = object
+  mainWindow: Window
+  mainUI: Container # メインウィンドウ最上位コンテナ
+  leftUI: Container # 左UI
+  rightUI: Container # 右UI
+  menuLabel: Label # 左UI->メニュー
+  beatmapMoveButton: Button # 左UI->譜面移動ボタン
+  resultArea: TextArea # 右UI->結果表示エリア
+  progressBar: ProgressBar # 右UI->プログレスバー
 
-proc makeNewWindow(name:string="", height:int=100, width:int=100, x:int=100, y:int=100): any =
-  result = newWindow(name)
-  result.height = height
-  result.width = width
-  result.x = x
-  result.y = y
+  settingWindow: Window
+  settingUI: Container # 設定ウィンドウ最上位コンテナ
+
+# UIのコンストラクタ
+proc newUI(): UI
+# ウィンドウ作成
+proc initWindow(self:var UI, name:string="", height:int=100, width:int=100, x:int=100, y:int=100): void
+# ウィンドウにコントロール追加
+proc addControls(self:var UI): void
+# ボタンの処理
+proc setButtonEvent(self:UI)
 
 proc main(): void =
   app.init()
-  let mainWindow = makeNewWindow("メインウィンドウ", 300, 500)
+  let ui = newUI()
 
-  let con = newLayoutContainer(Layout_Vertical)
-  con.setPosition(100,130)
-  mainWindow.add(con)
-
-  mainWindow.show()
+  ui.mainWindow.show()
   app.run()
 
+proc newUI(): UI =
+  result.initWindow("メインウィンドウ", 400, 600)
+  result.addControls()
+
+proc initWindow(self:var UI, name:string="", height:int=100, width:int=100, x:int=100, y:int=100): void =
+  self.mainWindow = newWindow(name)
+  self.mainWindow.height = height.scaleToDpi
+  self.mainWindow.width = width.scaleToDpi
+  self.mainWindow.x = x
+  self.mainWindow.y = y
+
+  self.settingWindow = newWindow("Setting")
+
+proc addControls(self:var UI): void =
+  self.mainUI = newLayoutContainer(Layout_Horizontal)
+  self.mainWindow.add(self.mainUI)
+
+  self.leftUI = newLayoutContainer(Layout_Vertical)
+  self.mainUI.add(self.leftUI)
+
+  self.menuLabel = newLabel("~Menu~")
+  self.leftUI.add(self.menuLabel)
+
+  self.beatmapMoveButton = newButton("Move beatmaps")
+  self.leftUI.add(self.beatmapMoveButton)
+
+  self.rightUI = newLayoutContainer(Layout_Vertical)
+  self.mainUI.add(self.rightUI)
+
+  self.resultArea = newTextArea(">>Start program.")
+  self.rightUI.add(self.resultArea)
+
+  self.progressBar =newProgressBar()
+  self.rightUI.add(self.progressBar)
+
+proc setButtonEvent(self:UI): void =
+  discard
 
 when isMainModule:
   main()
