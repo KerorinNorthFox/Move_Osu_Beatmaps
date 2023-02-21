e910235ba6366d19d87cea249851d66ce27412fc
kerorinnf <kitakitsune.gingitsune.8220A@gmail.com>
2023-02-21 23:13:55 +0900

    ウィンドウ別にクラスを分けた

--- src/gui.nim
+++ src/gui.nim
@@ -2,49 +2,52 @@ import
   nigui,
   ./utils
 
-type UI = object
+type UI = object of RootObj
   mainWindow: Window
-  mainUI: Container # メインウィンドウ最上位コンテナ
+  mainUI: Container # 画面最上位コンテナ
+
+# 設定画面のUI
+type SettingUI = object of UI
+  label: Label
+  whereToSave: SelectDirectoryDialog
+
+# メイン画面のUI
+type MainUI = object of UI
   leftUI: Container # 左UI
-  rightUI: Container # 右UI
   menuLabel: Label # 左UI->メニュー
   beatmapMoveButton: Button # 左UI->譜面移動ボタン
+
+  rightUI: Container # 右UI
   resultArea: TextArea # 右UI->結果表示エリア
   progressBar: ProgressBar # 右UI->プログレスバー
 
-  settingWindow: Window
-  settingUI: Container # 設定ウィンドウ最上位コンテナ
+  settingUI: SettingUI
 
-# UIのコンストラクタ
-proc newUI(): UI
-# ウィンドウ作成
-proc initWindow(self:var UI, name:string="", height:int=100, width:int=100, x:int=100, y:int=100): void
-# ウィンドウにコントロール追加
-proc addControls(self:var UI): void
-# ボタンの処理
-proc setButtonEvent(self:UI)
+proc newUI(): MainUI # UIのコンストラクタ
+proc initWindow(self:var MainUI, name:string="", height:int=100, width:int=100, x:int=100, y:int=100): void # ウィンドウ作成
+proc addControls(self:var MainUI): void # ウィンドウにコントロール追加
+proc setButtonEvent(self:MainUI) # ボタンの処理
 
 proc main(): void =
   app.init()
   let ui = newUI()
-
   ui.mainWindow.show()
   app.run()
 
-proc newUI(): UI =
+proc newUI(): MainUI =
   result.initWindow("メインウィンドウ", 400, 600)
   result.addControls()
 
-proc initWindow(self:var UI, name:string="", height:int=100, width:int=100, x:int=100, y:int=100): void =
+proc initWindow(self:var MainUI, name:string="", height:int=100, width:int=100, x:int=100, y:int=100): void =
   self.mainWindow = newWindow(name)
   self.mainWindow.height = height.scaleToDpi
   self.mainWindow.width = width.scaleToDpi
   self.mainWindow.x = x
   self.mainWindow.y = y
 
-  self.settingWindow = newWindow("Setting")
+  # self.settingWindow = newWindow("Setting")
 
-proc addControls(self:var UI): void =
+proc addControls(self:var MainUI): void =
   self.mainUI = newLayoutContainer(Layout_Horizontal)
   self.mainWindow.add(self.mainUI)
 
@@ -66,7 +69,7 @@ proc addControls(self:var UI): void =
   self.progressBar =newProgressBar()
   self.rightUI.add(self.progressBar)
 
-proc setButtonEvent(self:UI): void =
+proc setButtonEvent(self:MainUI): void =
   discard
 
 when isMainModule:
