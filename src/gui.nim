5b61243c60e3dcf76bef225b985f0befb2d0d6c9
kerorinnf <kitakitsune.gingitsune.8220A@gmail.com>
2023-02-21 23:40:58 +0900

    メイン画面と設定画面でクラス分けした

--- src/gui.nim
+++ src/gui.nim
@@ -3,13 +3,14 @@ import
   ./utils
 
 type UI = object of RootObj
-  mainWindow: Window
+  mainWindow: Window # メイン画面
   mainUI: Container # 画面最上位コンテナ
 
 # 設定画面のUI
 type SettingUI = object of UI
-  label: Label
-  whereToSave: SelectDirectoryDialog
+  label: Label # 題名
+  changeDirectoryButton: Button # 移動先のディレクトリを選択するボタン
+  saveButton: Button # 設定を保存するボタン
 
 # メイン画面のUI
 type MainUI = object of UI
@@ -21,12 +22,12 @@ type MainUI = object of UI
   resultArea: TextArea # 右UI->結果表示エリア
   progressBar: ProgressBar # 右UI->プログレスバー
 
-  settingUI: SettingUI
+  settingUI: SettingUI # 設定ウィンドウ
 
 proc newUI(): MainUI # UIのコンストラクタ
-proc initWindow(self:var MainUI, name:string="", height:int=100, width:int=100, x:int=100, y:int=100): void # ウィンドウ作成
-proc addControls(self:var MainUI): void # ウィンドウにコントロール追加
-proc setButtonEvent(self:MainUI) # ボタンの処理
+method initWindow(self:var UI, title:string="", height:int=100, width:int=100): void {.base.}  # ウィンドウ作成
+method addControls(self:var UI): void {.base.}  # ウィンドウにコントロール追加
+proc setButtonEvent(self: MainUI): void # ボタンの処理
 
 proc main(): void =
   app.init()
@@ -35,19 +36,31 @@ proc main(): void =
   app.run()
 
 proc newUI(): MainUI =
-  result.initWindow("メインウィンドウ", 400, 600)
+  result.initWindow("Main Window", 400, 600) # メイン画面作成
+  result.settingUI.initWindow("Setting", 200, 200) # 設定画面作成
   result.addControls()
+  result.settingUI.addControls()
 
-proc initWindow(self:var MainUI, name:string="", height:int=100, width:int=100, x:int=100, y:int=100): void =
-  self.mainWindow = newWindow(name)
+method initWindow(self:var UI, title:string="", height:int=100, width:int=100): void {.base.} =
+  self.mainWindow = newWindow(title)
   self.mainWindow.height = height.scaleToDpi
   self.mainWindow.width = width.scaleToDpi
-  self.mainWindow.x = x
-  self.mainWindow.y = y
 
-  # self.settingWindow = newWindow("Setting")
+method addControls(self:var UI): void {.base.} =
+  self.mainUI = newLayoutContainer(Layout_Horizontal)
+  self.mainWindow.add(self.mainUI)
 
-proc addControls(self:var MainUI): void =
+method addControls(self:var SettingUI): void =
+  self.mainUI = newLayoutContainer(Layout_Horizontal)
+  self.mainWindow.add(self.mainUI)
+
+  self.label = newLabel("hogehoge")
+  self.mainUI.add(self.label)
+
+  self.saveButton = newButton("Change save directory")
+  self.mainUI.add(self.saveButton)
+
+method addControls(self:var MainUI): void =
   self.mainUI = newLayoutContainer(Layout_Horizontal)
   self.mainWindow.add(self.mainUI)
 
@@ -69,7 +82,7 @@ proc addControls(self:var MainUI): void =
   self.progressBar =newProgressBar()
   self.rightUI.add(self.progressBar)
 
-proc setButtonEvent(self:MainUI): void =
+proc setButtonEvent(self: MainUI): void =
   discard
 
 when isMainModule:
